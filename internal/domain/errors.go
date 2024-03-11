0c698644aa6e229bf8642ca022db5c30fff6d809
reinhardjs <reinhardjsilalahi@gmail.com>
2024-03-11 20:35:07 +0700

    feat: added errors domain

---
+++ internal/domain/errors.go
@@ -0,0 +1,10 @@
+package domain
+
+import "errors"
+
+var (
+	ErrInternalServerError = errors.New("internal server error")
+	ErrNotFound            = errors.New("user is not found")
+	ErrConflict            = errors.New("username exist")
+	ErrBadParamInput       = errors.New("given param is not valid")
+)
