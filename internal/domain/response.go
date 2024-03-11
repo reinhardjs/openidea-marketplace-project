4b19d45e485204b0b6010245a8df961f6f2cae33
reinhardjs <reinhardjsilalahi@gmail.com>
2024-03-11 20:35:26 +0700

    feat: added success and error response domain

---
+++ internal/domain/response.go
@@ -0,0 +1,10 @@
+package domain
+
+type ResponseError struct {
+	Message string `json:"message"`
+}
+
+type ResponseSuccess struct {
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
