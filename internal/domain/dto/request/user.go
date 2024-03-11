fc10e6eee9e3ac818a47a1e8e44b6bb5e590d56c
reinhardjs <reinhardjsilalahi@gmail.com>
2024-03-11 20:59:34 +0700

    feat: added missing property on RegisterUserRequest

--- internal/domain/dto/request/user.go
+++ internal/domain/dto/request/user.go
@@ -1,6 +1,7 @@
 package request
 
 type RegisterUserRequest struct {
+	Name     string `json:"name" validate:"required"`
 	Username string `json:"id" validate:"required"`
 	Password string `json:"password" validate:"required"`
 }
