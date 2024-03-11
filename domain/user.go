7df2ba35467753ee75dbcc7e3c6f4608dec8427b
reinhardjs <reinhardjsilalahi@gmail.com>
2024-03-11 20:06:19 +0700

    feat: added user domain

---
+++ domain/user.go
@@ -0,0 +1,23 @@
+package domain
+
+import (
+	"context"
+
+	"github.com/openidea-marketplace/domain/dto/request"
+	"github.com/openidea-marketplace/domain/dto/response"
+)
+
+type User struct {
+	ID       int64  `json:"id"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type UserUsecase interface {
+	Register(ctx context.Context, request *request.RegisterUserRequest) ([]response.RegisterUserResponse, error)
+}
+
+type UserRepository interface {
+	Register(ctx context.Context, request *request.RegisterUserRequest) error
+}
