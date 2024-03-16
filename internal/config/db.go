4ec16e5f4e77301da45641e52f73fa9a527bc5eb
reinhardjs <reinhardjsilalahi@gmail.com>
2024-03-16 22:12:11 +0700

    added db config

---
+++ internal/config/db.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/url"
+
+	"github.com/spf13/viper"
+)
+
+func NewDatabase(viper *viper.Viper) *sql.DB {
+	//prepare database
+	dbHost := viper.GetString("database.host")
+	dbPort := viper.GetString("database.port")
+	dbUser := viper.GetString("database.user")
+	dbPass := viper.GetString("database.password")
+	dbName := viper.GetString("database.name")
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	val := url.Values{}
+	val.Add("parseTime", "1")
+	val.Add("loc", "Asia/Jakarta")
+	dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
+	dbConn, err := sql.Open(`mysql`, dsn)
+	if err != nil {
+		log.Fatal("failed to open connection to database", err)
+	}
+	err = dbConn.Ping()
+	if err != nil {
+		log.Fatal("failed to ping database ", err)
+	}
+
+	return dbConn
+}
