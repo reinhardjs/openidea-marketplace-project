0c0d1d4c1d7b193c39584a90732239f637fc712e
reinhardjs <reinhardjsilalahi@gmail.com>
2024-03-18 02:08:55 +0700

    feat: updated argon2id hashing

--- pkg/utils/hashing/argon2id.go
+++ pkg/utils/hashing/argon2id.go
@@ -1,28 +1,28 @@
 package hashing
 
 import (
-	"bytes"
 	"crypto/rand"
+	"crypto/subtle"
+	"encoding/base64"
 	"errors"
+	"fmt"
+	"strings"
 
 	"golang.org/x/crypto/argon2"
 )
 
-type Hashing interface {
-	GenerateHash(password []byte) (*hashSalt, error)
-	Compare(hash, salt, password []byte) error
-}
+/**
+ * From: https://www.alexedwards.net/blog/how-to-hash-and-verify-passwords-with-argon2-in-go
+**/
 
-// HashSalt struct used to store
-// generated hash and salt used to
-// generate the hash.
-type hashSalt struct {
-	Hash, Salt []byte
+type Hashing interface {
+	GenerateHashFromPassword(password string) (string, error)
+	Compare(hash, password string) (bool, error)
 }
 
 type argon2idHash struct {
 	// time represents the number of
-	// passed over the specified memory.
+	// passed over the specified memory (iterations).
 	time uint32
 	// cpu memory to be used.
 	memory uint32
@@ -57,32 +57,75 @@ func randomSecret(length uint32) ([]byte, error) {
 	return secret, nil
 }
 
-// GenerateHash using the password and provided salt.
-// If not salt value provided fallback to random value
-// generated of a given length.
-func (a *argon2idHash) GenerateHash(password []byte) (*hashSalt, error) {
+func (a *argon2idHash) GenerateHashFromPassword(password string) (encodedHash string, err error) {
 	salt, err := randomSecret(a.saltLen)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	// Generate hash
-	hash := argon2.IDKey(password, salt, a.time, a.memory, a.threads, a.keyLen)
-	// Return the generated hash and salt used for storage.
-	return &hashSalt{Hash: hash, Salt: salt}, nil
+	hash := argon2.IDKey([]byte(password), salt, a.time, a.memory, a.threads, a.keyLen)
+
+	// Base64 encode the salt and hashed password.
+	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+
+	// Return a string using the standard encoded hash representation.
+	encodedHash = fmt.Sprintf("$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s", argon2.Version, a.memory, a.time, a.threads, b64Salt, b64Hash)
+
+	return encodedHash, nil
 }
 
 // Compare generated hash with store hash.
-func (a *argon2idHash) Compare(hash, salt, password []byte) error {
-	// Generate hash for comparison.
-	hashSalt, err := a.GenerateHash(password)
+func (a *argon2idHash) Compare(encodedHash, password string) (match bool, err error) {
+	p, salt, hash, err := decodeHash(encodedHash)
 	if err != nil {
-		return err
+		return
 	}
-	// Compare the generated hash with the stored hash.
-	// If they don't match return error.
-	if !bytes.Equal(hash, hashSalt.Hash) {
-		return errors.New("hash doesn't match")
+
+	// Generate hash for comparison.
+	generatedHash := argon2.IDKey([]byte(password), salt, p.time, p.memory, p.threads, p.keyLen)
+
+	// Check that the contents of the hashed passwords are identical. Note
+	// that we are using the subtle.ConstantTimeCompare() function for this
+	// to help prevent timing attacks.
+	if subtle.ConstantTimeCompare(hash, generatedHash) == 1 {
+		return true, nil
 	}
-	return nil
+	return false, nil
+}
+
+func decodeHash(encodedHash string) (p *argon2idHash, salt, hash []byte, err error) {
+	vals := strings.Split(encodedHash, "$")
+	if len(vals) != 6 {
+		return nil, nil, nil, errors.New("ErrInvalidHash")
+	}
+
+	var version int
+	_, err = fmt.Sscanf(vals[2], "v=%d", &version)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	if version != argon2.Version {
+		return nil, nil, nil, errors.New("ErrIncompatibleVersion")
+	}
+
+	p = &argon2idHash{}
+	_, err = fmt.Sscanf(vals[3], "m=%d,t=%d,p=%d", &p.memory, &p.time, &p.threads)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	salt, err = base64.RawStdEncoding.Strict().DecodeString(vals[4])
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	p.saltLen = uint32(len(salt))
+
+	hash, err = base64.RawStdEncoding.Strict().DecodeString(vals[5])
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	p.keyLen = uint32(len(hash))
+
+	return p, salt, hash, nil
 }
